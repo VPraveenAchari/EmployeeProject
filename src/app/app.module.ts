7ceec16b184317821123828d596daa322c125e42
PraveenAchari <praveen.achari@accionlabs.com>
2023-04-19 23:06:09 +0530

    authservices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { EmployeeDashboardComponent } from './components/employee-dashboard/employee-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    EmployeeDashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import ValidateForm from 'src/app/helpers/validateform';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,9 +14,7 @@ export class LoginComponent implements OnInit {
   isText: boolean = false
   eyeIcon: string = "fa-eye-slash";
   loginForm!: FormGroup;
-  constructor(private fb: FormBuilder) {
-
-  }
+  constructor(private fb: FormBuilder,private auth:AuthService,private router:Router) { }
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
@@ -21,30 +22,30 @@ export class LoginComponent implements OnInit {
     })
   }
   hideShowPass() {
-    
+
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password";
   }
-  onSubmit() {
+  onLogin() {
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
+      this.auth.login(this.loginForm.value).subscribe({
+        next:(res)=>{
+          alert(res.message)
+          this.loginForm.reset();
+          this.router.navigate(['employee-dashboard'])
+        },
+        error:(err)=>{
+          alert(err?.error.message)
+        }
+      })
     }
     else {
-      
-this.validateAllFormFields(this.loginForm);
-alert("Form is invalid");
+
+      ValidateForm.validateAllFormFields(this.loginForm);
+      alert("Form is invalid");
     }
   }
-  private validateAllFormFields(formGroup: FormGroup) {
-    Object.keys(formGroup.controls).forEach(field=>{
-      const control=formGroup.get(field);
-      if(control instanceof FormControl)
-      {
-        control.markAsDirty({onlySelf:true});
-      }else if(control instanceof FormGroup){
-        this.validateAllFormFields(control)
-      }
-    })
-  }
-    }
+ 
+}
